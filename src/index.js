b127e39f25ded335c909482ccff3fdba98215048
Cesar Martin <kreytos.pavi10@gmail.com>
2021-04-04 22:22:35 -0500

    workshop fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const API = 'https://platzi-avo.vercel.app/api/avo';
+const IMAGE_URL = 'https://platzi-avo.vercel.app';
+const containerNode = document.querySelector('div#container');
 
-console.log('Happy hacking :)')
+const fetchData = async () => {
+    const response = await fetch(API);
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+
+    const allItems = [];
+    responseJSON.data.forEach(element => {
+        
+        const container = document.createElement('div');
+        const img = document.createElement('img');
+        img.src = `${IMAGE_URL}${element.image}`;
+        const title = document.createElement('h3');
+        title.textContent = element.name;
+        const price = document.createElement('div');
+        price.textContent = element.price;
+
+        container.append(img,title,price);
+        allItems.push(container);
+    });
+    containerNode.append(...allItems);
+}
+
+fetchData();
